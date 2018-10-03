35624b87a1064938acb49c3fe15343304f57fe31
Robert Tremewan <robert_tremewan@umail.ucsb.edu>
2018-10-02 22:03:38 -0700

    first commit

---
+++ helloWorld.cpp
@@ -0,0 +1,10 @@
+// helloWorld.cpp  R. Wang for UCSB CS32 F18
+// Edited by: Robert Tremewan
+// minimal Hello World! program for testing Makefiles
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
